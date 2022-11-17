9e332d9a2a6f362624519c686a85d01b5b95ea22
DavidEcklund <davidecklund@protonmail.com>
2022-11-17 08:10:03 +0100

    Create Sentence component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import '../css/App.css';
-import React, { useState, useEffect } from "react";
+import Sentence from './Sentence';
+// import React, { useState, useEffect } from "react";
 
 function App() {
-  const [answer, setAnswer] = useState();
+  // const [answer, setAnswer] = useState();
 
-  const getAnswer = async () => {
-    const response = await fetch("http://localhost:3000/");
-    const objFromJSON = await response.json();
-    const answer = objFromJSON.msg; 
-    setAnswer(answer);
-  };
+  // const getAnswer = async () => {
+  //   const response = await fetch("http://localhost:3000/");
+  //   const objFromJSON = await response.json();
+  //   const answer = objFromJSON.msg; 
+  //   setAnswer(answer);
+  // };
   
   
-  useEffect(() => {
-    getAnswer();
-  }, []);
+  // useEffect(() => {
+  //   getAnswer();
+  // }, []);
   
   return (
     <>
-      {String(answer)}
+      <Sentence />
+      <Sentence />
     </>
   )
 }
---
+++ src/components/Sentence.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sentence() {
+  return (
+    <div>
+      Here is a Sentence
+    </div>
+  )
+}
